7710f7bcaeff2c0d9aa18250f4ab343dae371477
kyul <ulrichkamdem048@gmail.com>
2022-12-26 17:09:13 +0100

    page 2

---
+++ app/src/main/java/com/example/tp/CreateAccoumt.java
@@ -0,0 +1,14 @@
+package com.example.tp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccoumt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_accoumt);
+    }
+}
