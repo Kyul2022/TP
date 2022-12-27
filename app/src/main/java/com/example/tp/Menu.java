5d1f597e8344ab68796472c8c8035c1ac143354d
kyul <ulrichkamdem048@gmail.com>
2022-12-27 09:19:03 +0100

    ajout firease

--- app/src/main/java/com/example/tp/Menu.java
+++ app/src/main/java/com/example/tp/Menu.java
@@ -2,13 +2,17 @@ package com.example.tp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Menu extends AppCompatActivity {
-
+    ListView lv=null;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        lv=findViewById(R.id.listview);
     }
 }
